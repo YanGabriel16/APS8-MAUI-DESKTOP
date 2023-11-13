4e81bf7ec2dc3d07767e5e0cb539e81585e05563
Yan Gabriel Silva Queiroz <yan80ky16-gsq@hotmail.com>
2023-11-12 23:07:28 -0300

    Feat :: funcionalidade excluir, editar e adicionar local

--- Services/LocalService.cs
+++ Services/LocalService.cs
@@ -1,6 +1,8 @@
 ﻿using APS8_MAUI_DESKTOP.Objects;
+using APS8_MAUI_DESKTOP.Objects.Request;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System.Text;
 
 namespace APS8_MAUI_DESKTOP.Services
 {
@@ -52,6 +54,73 @@ namespace APS8_MAUI_DESKTOP.Services
                 {
                     throw new Exception($"Erro na requisição: {response.StatusCode}");
                 }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ocorreu um erro na requisição: {ex.Message}");
+            }
+        }
+
+        public async Task<EnderecoObject> GetEndereco(string cep)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync($"/api/Local/consulta/cep/{cep}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<EnderecoObject>(responseContent) ?? new EnderecoObject();
+                }
+                else
+                {
+                    throw new Exception($"Erro na requisição: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ocorreu um erro na requisição: {ex.Message}");
+            }
+        }
+
+        public async Task<bool> AdicionarLocal(AdicionarLocalRequest request)
+        {
+            try
+            {
+                StringContent body = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await _httpClient.PostAsync($"/api/Local", body);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ocorreu um erro na requisição: {ex.Message}");
+            }
+        }
+
+        public async Task<bool> AtualizarLocal(int id, AtualizarLocalRequest request)
+        {
+            try
+            {
+                var jsonData = JsonConvert.SerializeObject(request);
+                StringContent body = new(jsonData, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await _httpClient.PostAsync($"/api/Local/{id}", body);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ocorreu um erro na requisição: {ex.Message}");
+            }
+        }
+
+        public async Task<bool> ExcluirLocal(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.DeleteAsync($"/api/Local/{id}");
+
+                return response.IsSuccessStatusCode;
             }
             catch (Exception ex)
             {
