52a6561fb07b3303f0d8e0d08ada8370920942c5
Yan Gabriel Silva Queiroz <yan80ky16-gsq@hotmail.com>
2023-11-13 23:56:58 -0300

    Feat :: graficos

--- Utils/DataUtils.cs
+++ Utils/DataUtils.cs
@@ -1,14 +1,45 @@
-﻿using Radzen;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace APS8_MAUI_DESKTOP.Utils
+﻿namespace APS8_MAUI_DESKTOP.Utils
 {
-    public static class DataUtils
+    using System;
+    using System.Collections.Generic;
+
+    public class DataUtils
     {
+        public static List<string> ObterUltimos15DiasString()
+        {
+            List<string> ultimos15Dias = new();
+
+            for (int i = 14; i >= 0; i--)
+            {
+                DateTime data = DateTime.Now.AddDays(-i);
+                string dia = data.Day.ToString().PadLeft(2, '0');
+                string mes = data.Month.ToString().PadLeft(2, '0');
+                string dataFormatada = $"{dia}/{mes}";
+                ultimos15Dias.Add(dataFormatada);
+            }
+
+            return ultimos15Dias;
+        }
+
+        public static List<DateTime> ObterUltimos15Dias()
+        {
+            List<DateTime> ultimos15Dias = new();
+
+            for (int i = 14; i >= 0; i--)
+            {
+                DateTime data = DateTime.Now.AddDays(-i);
+                ultimos15Dias.Add(data);
+            }
+
+            return ultimos15Dias;
+        }
+
+        public static string FormatarData(DateTime data)
+        {
+            return data.ToString("dd/MM/yyyy");
+        }
+
         public static string FormatFrase(DateTime data) => data.ToString("d 'de' MMMM 'de' yyyy 'às' HH:mm");
+        public static string FormatDiaMes(object value) => value != null ? Convert.ToDateTime(value).ToString("dd/MM") : string.Empty;
     }
 }
