49b91554eb1e0a4f15514ca1dd3f64c53d51949a
Yan Gabriel Silva Queiroz <yan80ky16-gsq@hotmail.com>
2023-11-13 21:19:07 -0300

    Fix :: removido airquality

--- Objects/LocalDadosObject.cs
+++ Objects/LocalDadosObject.cs
@@ -2,7 +2,6 @@ namespace APS8_MAUI_DESKTOP.Objects
 {
     public class LocalDadosObject
     {
-        public AirQualityResponse QualidadeAr { get; set; }
         public OpenWeatherResponse Clima { get; set; }
         public DateTime Data { get; set; }
     }
